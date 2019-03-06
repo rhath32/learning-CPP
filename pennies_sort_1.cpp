fa53a4cfe0dc78b5ff77a81de0bf88d90def4d2e
rhath32 <45647039+rhath32@users.noreply.github.com>
2019-03-06 10:04:46 -0800

    Update pennies_sort_1.cpp
    This program takes an integer input of pennies from the user, and separates them into larger currency denominations. It also calculates the value in Canadian dollar using the current exchange rate(1 USD = 1.33 Canadian). It then outputs the results.

--- pennies_sort_1.cpp
+++ pennies_sort_1.cpp
@@ -16,41 +16,41 @@ string breakCash(int numPennies)
  * FUNCTION SINGATURE:
  * string breakCash(int numPennies)
  */
-	// calculate Canadian dollar amount
-	float canadianTotal = numPennies * .0133;
+    // calculate Canadian dollar amount
+    float canadianTotal = numPennies * .0133;
 
-	// split total amount of pennies into dollars, quarters, nickels, dimes, and pennies
+    // split total amount of pennies into dollars, quarters, nickels, dimes, and pennies
 
     int dollars = numPennies / 100;
     numPennies = numPennies % 100;
 
-	int quarters = numPennies / 25;
-	numPennies = numPennies % 25;
+    int quarters = numPennies / 25;
+    numPennies = numPennies % 25;
 
-	int dimes = numPennies / 10;
-	numPennies = numPennies % 10;
+    int dimes = numPennies / 10;
+    numPennies = numPennies % 10;
 
-	int nickels = numPennies / 5;
-	numPennies = numPennies % 5;
+    int nickels = numPennies / 5;
+    numPennies = numPennies % 5;
 
-	int pennies = numPennies;
+    int pennies = numPennies;
 
-	// display results
-	std::stringstream ss;
-	ss << "In US currency you have: " << dollars << " dollars, " << quarters << " quarters, " << dimes << " dimes, "
+    // display results
+    std::stringstream ss;
+    ss << "In US currency you have: " << dollars << " dollars, " << quarters << " quarters, " << dimes << " dimes, "
         << nickels << " nickels, and " << pennies << " pennies." << endl << "In Canadian currency you have: $"
         << std::fixed << setprecision(2) << canadianTotal << endl;
-	return ss.str();
+    return ss.str();
 }
 
 int main()
 {
   int numOfPennies = 0;
   // retrieve amount of pennies from user
-	cout << "Enter all of your pennies:" << endl;
-	cin >> numOfPennies;
-	cout << breakCash(numOfPennies);
-	return 0;
+  cout << "Enter all of your pennies:" << endl;
+  cin >> numOfPennies;
+  cout << breakCash(numOfPennies);
+  return 0;
 }
 
 /*
