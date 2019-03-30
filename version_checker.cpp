8f92ceab9391a4ce1ddaf01d2e1419802f513dcf
rhath32 <45647039+rhath32@users.noreply.github.com>
2019-03-30 12:33:57 -0700

    Update version_checker.cpp
    Finally have this version checker program working. I know there must be ways to make this system more compact and efficient, so I will continue to work on efficiency.

--- version_checker.cpp
+++ version_checker.cpp
@@ -22,8 +22,11 @@ int main()
     cout << compareVersions("300", "299.9.9") << endl; // 1
     cout << compareVersions("5.1.1", "4") << endl; // 1
     cout << compareVersions("300.23.15", "256") << endl; // 1
-    cout << compareVersions("301.23", "301.2.1") << endl; // 0
-    cout << compareVersions(".48.7", ".47.8") << endl; // 1
+    cout << compareVersions("301.23.1", "301.23.1") << endl; // 0
+    cout << compareVersions("850", "850.0.0") << endl; // 0
+    cout << compareVersions(".0.1", ".0.2") << endl; // -1
+    cout << compareVersions("3.1.1", "4") << endl; // -1
+    cout << compareVersions("1.1", "1") << endl; // 1
     return 0;
 }
 
@@ -43,8 +46,9 @@ int main()
 int compareVersions(string ver1, string ver2)
 { 
     int dotCounter = 0, firstDot = 0, secondDot = 0, previousDecimal = 0, ver1Major = 0, ver1Minor = 0, 
-        ver1Revision = 0, ver2Major = 0, ver2Minor = 0, ver2Revision = 0;
+        ver1Revision = 0, ver2Major = 0, ver2Minor = 0, ver2Revision = 0, comparisonResult = 0;
 
+    // VERSION 1 ---------
     // store dot positions
     for (int i = 0; i < ver1.size(); i++)
     {
@@ -57,8 +61,7 @@ int compareVersions(string ver1, string ver2)
                 secondDot = i;
         }
     }
-
-    // VERSION 1: store major, minor, and revision into integer variables
+    // store major, minor, and revision into integer variables
     if (dotCounter == 0)
     {
         ver1Major = stoi(ver1);
@@ -85,13 +88,73 @@ int compareVersions(string ver1, string ver2)
         ver1Minor = stoi(ver1.substr(firstDot + 1, secondDot - (firstDot + 1)));
         ver1Revision = stoi(ver1.substr(secondDot + 1, ver1.size() - secondDot));
     }
-    
 
+    // VERSION 2 ---------
+    // store dot positions
+    dotCounter = 0;
+    firstDot = 0;
+    secondDot = 0;
 
-    
-        cout << ver1Major << "." << ver1Minor << "." << ver1Revision << endl;
+    for (int i = 0; i < ver2.size(); i++)
+    {
+        if (ver2[i] == '.')
+        {
+            dotCounter++;
+            if (dotCounter == 1)
+                firstDot = i;
+            else if (dotCounter == 2)
+                secondDot = i;
+        }
+    }
+    // store major, minor, and revision into integer variables
+    if (dotCounter == 0)
+    {
+        ver2Major = stoi(ver2);
+        ver2Minor = 0;
+        ver2Revision = 0;
+    }
+    else if (dotCounter == 1)
+    {
+        if (firstDot == 0)
+            ver2Major = 0;
+        else
+            ver2Major = stoi(ver2.substr(0, firstDot));
+        
+        ver2Minor = stoi(ver2.substr(firstDot + 1, ver2.size() - firstDot));
+        ver2Revision = 0;
+    }
+    else if (dotCounter == 2)
+    {
+        if (firstDot == 0)
+            ver2Major = 0;
+        else
+            ver2Major = stoi(ver2.substr(0, firstDot));
+        
+        ver2Minor = stoi(ver2.substr(firstDot + 1, secondDot - (firstDot + 1)));
+        ver2Revision = stoi(ver2.substr(secondDot + 1, ver2.size() - secondDot));
+    }
 
+    // ------ Comparison Test Cases -------
+    // If ver1 > ver2: return 1
+    // if ver1 < ver2: return -1
+    // otherwise, return 0. 
+    if (ver1Major > ver2Major)
+        comparisonResult = 1;
+    else if (ver1Major < ver2Major)
+        comparisonResult = -1;
+    else if (ver1Major == ver2Major)
+    {
+        if (ver1Minor > ver2Minor)
+            comparisonResult = 1;
+        else if (ver1Minor < ver2Minor)
+            comparisonResult = -1;
+        else if ((ver1Minor == ver2Minor) && (ver1Revision == ver2Revision))
+            comparisonResult = 0;
+        else if ((ver1Minor == ver2Minor) && (ver1Revision > ver2Revision))
+            comparisonResult = 1;
+        else if ((ver1Minor == ver2Minor) && (ver1Revision < ver2Revision))
+            comparisonResult = -1;
+    }
 
-
-    //return firstDot;
+    return comparisonResult;
 }
