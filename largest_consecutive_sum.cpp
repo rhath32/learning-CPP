6a6d1eeee6d0a8aaf7c73d166ad74b1f89baf99e
rhath32 <45647039+rhath32@users.noreply.github.com>
2019-05-10 10:33:45 -0700

    Update largest_consecutive_sum.cpp

--- largest_consecutive_sum.cpp
+++ largest_consecutive_sum.cpp
@@ -1,7 +1,12 @@
-// DYNAMIC PROGRAMMING 
+/** This is a simple example of a Kadane's dynamic programming algorithm.
+*   This algorithm iterates through the array and looks for all POSITIVE contigous segments.
+*   For each positive contigous segment, we keep track of the sum of the segment.
+*   Each time we get a positive sum, compare it to previous positive MAX sum. 
+*   If it is larger than previous positive MAX sum, set as new MAX sum.
+*/
 #include<iostream>
 
-int getLargestConsecutiveSum(const int[], int);
+int getLargestConsecutiveSum(const int[], int); 
 
 int main() 
 {
@@ -24,17 +29,17 @@ int main()
  */
 int getLargestConsecutiveSum(const int arr[], int length) 
 {
-	int currentMax = 0, finalMax = 0;
+	int currentSum = 0, finalSum = 0;
 
-	for (int i = 0; i < length; i++)
+	for (int i = 0; i < length; i++) // loop through array
 	{
-		currentMax += arr[i];
+		currentSum += arr[i]; // add current element to currentMAx
 
-		if (finalMax < currentMax)
-			finalMax = currentMax;
+		if (finalSum < currentSum) // if currentSum is larger than finalSum
+			finalSum = currentSum; // set currentSum to finalSum
 
-		if (currentMax < 0)
-			currentMax = 0;
+		if (currentSum < 0) // if currentSum is negative, set to 0
+			currentSum = 0;
 	}
-	return finalMax;
+	return finalSum;
 }
